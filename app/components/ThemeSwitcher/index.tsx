d45b32c72bf31d755ce174593e3090ce16c00483
Pritam Dhara <46365255+pritam1813@users.noreply.github.com>
2024-02-12 13:43:47 +0530

    added themeswitcher button

---
+++ app/components/ThemeSwitcher/index.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import { Switch } from "@nextui-org/react";
+import MoonIcon from "./MoonIcon";
+import SunIcon from "./SunIcon";
+
+const ThemeSwitcher = () => {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  return (
+    <Switch
+      defaultSelected={theme === "dark"}
+      size="lg"
+      thumbIcon={({ isSelected, className }) =>
+        isSelected ? (
+          <SunIcon className={className} />
+        ) : (
+          <MoonIcon className={className} />
+        )
+      }
+      onChange={() => setTheme(theme === "dark" ? "light" : "dark")}
+    />
+  );
+};
+
+export default ThemeSwitcher;
