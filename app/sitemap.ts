c95d11820a649792ea3f809cc54db9de7b37351e
Pritam Dhara <46365255+pritam1813@users.noreply.github.com>
2024-03-01 11:33:09 +0530

    created sitemap and robots.txt file

---
+++ app/sitemap.ts
@@ -0,0 +1,45 @@
+import { MetadataRoute } from "next";
+import hygraph from "@/app/lib/hygraph";
+
+interface SiteMapProps {
+  slug: string;
+  updatedAt: string;
+}
+
+const generateSitemapObjects = async () => {
+  const { posts, pages }: { posts: SiteMapProps[]; pages: SiteMapProps[] } =
+    await hygraph.request(
+      `{
+          pages(orderBy: createdAt_DESC) {
+            slug
+            updatedAt
+          }
+          posts(orderBy: createdAt_DESC) {
+            slug
+            updatedAt
+          }
+        }`
+    );
+
+  return { posts, pages };
+};
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  return [
+    {
+      url: `${process.env.VERCEL_URL}`,
+    },
+    {
+      url: `${process.env.VERCEL_URL}/posts`,
+      changeFrequency: "weekly",
+    },
+    ...(await generateSitemapObjects()).posts.map((post) => ({
+      url: `${process.env.VERCEL_URL}/posts/${post.slug}`,
+      lastmod: post.updatedAt,
+    })),
+    ...(await generateSitemapObjects()).pages.map((page) => ({
+      url: `${process.env.VERCEL_URL}/${page.slug}`,
+      lastmod: page.updatedAt,
+    })),
+  ];
+}
